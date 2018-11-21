240766300c89e20bca3aac1a31c030c41d248274
andrewZhao1 <273701592@qq.com>
2018-11-21 22:46:50 +0800

    register and login

--- chatApp/urls.py
+++ chatApp/urls.py
@@ -5,4 +5,6 @@ app_name = '[chatApp]'
 urlpatterns = [
     path('index/', views.index),
     path('register/', views.register, name='register'),
+    path('register/action', views.register_action, name='register_action'),
+    path('index/login', views.login_action, name='login_action'),
 ]
--- chatApp/views.py
+++ chatApp/views.py
@@ -8,3 +8,22 @@ def index(request):
 
 def register(request):
 	return render(request, 'chatApp/register.html')
+
+def login_action(request):
+	phone = request.POST.get('phone')
+	password = request.POST.get('password')
+	user = models.user.objects.get(userPhoneNum=phone)
+	if user and user.userPw == password:
+		return render(request, 'chatApp/main.html', {'user':user})
+	else:
+		return render(request, 'chatApp/index.html', {'message':'用户不存在或密码错误'})
+
+
+
+def register_action(request):
+	name = request.POST.get('name')
+	phone = request.POST.get('phone')
+	password = request.POST.get('password')
+	age = request.POST.get('age')
+	models.user.objects.create(userName=name, userPhoneNum=phone, userPw=password, userAge=age)
+	return render(request, 'chatApp/index.html')
